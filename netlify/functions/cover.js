99b72a83029e0e61bc91df57576baf3384c46777
extsup <ciklah7878@gmail.com>
2026-03-13 21:43:53 +0700

    Update cover.js

--- netlify/functions/cover.js
+++ netlify/functions/cover.js
@@ -87,7 +87,6 @@ function fetchImage(url, referer) {
       headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Referer": referer,
-        "Origin": referer,
         "Accept": "image/avif,image/webp,image/apng,image/*,*/*;q=0.8",
         "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
         "Sec-Fetch-Dest": "image",
